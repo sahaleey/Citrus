e011cf0c098e84498432e896cc876677a4969387
crazydrace <ajua46244@gmail.com>
2025-10-25 22:16:37 +0530

    cors api allowed

--- smart-dine-backend/app.js
+++ smart-dine-backend/app.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: ["https://citrus-jucery.vercel.app", "http://localhost:5173"],
+    origin: ["https://citrus-juicerie.vercel.app", "http://localhost:5173"],
   })
 );
 app.use(express.json());
